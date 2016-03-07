e4c9ad192bf0b590deb70370621bf63aa9bcae01
Rushikesh Joshi <rsjoshi@caltech.edu>
2016-03-07 03:39:46 -0800

    Random stuff

---
+++ parse_data.py
@@ -0,0 +1,11 @@
+'''
+CS155 Mini-Project: ShakespeareanBot 5000
+Rushikesh Joshi and KC Emezie
+Winter, 2016
+
+In this project we will train an HMM on the entire corpus of Shakespearean 
+sonnets and then attempt to generate our own Shakespearean sonnet.
+'''
+
+import numpy as np
+
